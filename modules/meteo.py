30fb606a64110d97560449938ba94f253beb1567
Rmo_le_goat <ahondoromeo2@gmail.com>
2026-08-06 14:56:23 +0000

    Fetch real-time weather data from OpenWeather API
    Updated get_meteo function to fetch real-time weather data from OpenWeather API.

--- modules/meteo.py
+++ modules/meteo.py
@@ -1,7 +1,15 @@
-def get_meteo():
-    # Exemple simple : on retourne une météo fictive
-    return {
-        "ville": "Lomé",
-        "temperature": "28°C",
-        "condition": "Ensoleillé ☀️"
-    }
+import requests
+from config import API_KEY_OPENWEATHER
+
+def get_meteo(city="Lomé"):
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={API_KEY_OPENWEATHER}&lang=fr"
+    response = requests.get(url).json()
+
+    if response.get("cod") == 200:
+        return {
+            "ville": response["name"],
+            "temperature": f"{response['main']['temp']}°C",
+            "condition": response["weather"][0]["description"].capitalize()
+        }
+    else:
+        return {"ville": city, "temperature": "N/A", "condition": "Ville introuvable"}
